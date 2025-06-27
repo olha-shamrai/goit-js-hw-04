eea0817ed7734aa50e2aab1a5e66cf8c19c8ecf0
Olha Shamrai <olga.moryacka91@gmail.com>
2025-06-28 01:52:10 +0300

    add task 1

--- js/task-1.js
+++ js/task-1.js
@@ -8,7 +8,18 @@
 // Тобто порахувати загальну кількість товарів в об'єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
 function isEnoughCapacity(products, containerSize) {
+    let totalQuantity = 0;
 
+    const quantityValues = Object.values(products);
+    for (const quantity of quantityValues) {
+        totalQuantity += quantity;
+    }
+
+    if (totalQuantity <= containerSize) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
