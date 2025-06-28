71301ceff9ae169b47693f9e6414012b08b7584e
Olha Shamrai <olga.moryacka91@gmail.com>
2025-06-28 22:40:43 +0300

    add task 3

--- js/task-3.js
+++ js/task-3.js
@@ -5,6 +5,19 @@
 const profile = {
     username: "Jacob",
     playTime: 300,
+
+
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
 };
 
 // Доповни об'єкт profile методами для роботи з його властивостями.
